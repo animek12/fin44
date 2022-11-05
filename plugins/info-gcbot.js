b330fbd5faef11981aab993389934ee0cb6a6103
animek12 <111511065+animek12@users.noreply.github.com>
2022-11-05 11:34:32 +0800

    Update info-gcbot.js

--- plugins/info-gcbot.js
+++ plugins/info-gcbot.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
  let handler = async(m, { conn }) => { 
 
          let pp = fs.readFileSync('./thumbnail.jpg')
-         let gc1 = 'https://bit.ly/3ESNUnD'
+         let gc1 = 'https://chat.whatsapp.com/KwmT6WBRghjBTHrxpxWNre'
          let caption = `*Mʏ Gᴄ Oғғɪᴄɪᴀʟ*`
          await conn.sendButtonDoc(m.chat, caption, wm, 'Owner', '.owner', m, { contextInfo: { 
              externalAdReply: {
